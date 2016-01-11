eac871469968bd8e888b17114223a1dad0228655
scrachatron <scrachatron@googlemail.com>
2016-01-11 17:09:57 +0000

    Fixed player drawing Movment done in steps rather than in velocities

--- once/GameCode/Actor.cs
+++ once/GameCode/Actor.cs
@@ -21,44 +21,34 @@ namespace Once
             }
 
         }
-        public Vector2 Position
+        public Point Position
         {
             get { return m_pos; }
             set
             {
                 m_pos = value;
-                m_rect.X = (int)value.X;
-                m_rect.Y = (int)value.Y;
             }
         }
 
         private Color m_tint;
         private Rectangle m_rect;
-        private Vector2 m_pos;
+        private Point m_pos;
 
-        private Vector2 m_vel;
         protected Point MoveHere = new Point(0, 0);
-        protected Point IAM = new Point(0, 0);
 
         public Actor(Rectangle rect, Color tint, int layer)
         {
             Rect = rect;
-            m_vel = Vector2.Zero;
             m_tint = tint;
         }
         public virtual void UpdateMe(GameTime gt, Level level)
         {
             m_rect.Width = level.LayerSize.X;
             m_rect.Height = level.LayerSize.Y;
-            m_rect.X = (int)m_pos.X;
-            m_rect.Y = (int)m_pos.Y;
-
-            IAM.X = (int)((m_pos.X + m_rect.Width/2) / level.LayerSize.X);
-            IAM.Y = (int)((m_pos.Y + m_rect.Height / 2) / level.LayerSize.Y);
+            m_rect.X = (int)m_pos.X * level.LayerSize.X;
+            m_rect.Y = (int)m_pos.Y * level.LayerSize.Y;
 
             Collision(level);
-            //Position += m_vel;
-            m_vel = Vector2.Zero;
         }
         public virtual void DrawMe(SpriteBatch sb)
         {
@@ -68,18 +58,18 @@ namespace Once
         {
             if (MoveHere.X != 0 && MoveHere.Y != 0)
             {
-                if (lvl.Map[IAM.X + MoveHere.X,IAM.Y] == 0
-                    && lvl.Map[IAM.X, IAM.Y + MoveHere.Y] == 0)
-                    if (lvl.Map[IAM.X + MoveHere.X, IAM.Y + MoveHere.Y] == 0)
+                if (lvl.Map[Position.X + MoveHere.X,Position.Y] == 0
+                    && lvl.Map[Position.X, Position.Y + MoveHere.Y] == 0)
+                    if (lvl.Map[Position.X + MoveHere.X, Position.Y + MoveHere.Y] == 0)
                     {
-                        m_pos += MoveHere.ToVector2() * 2;
+                        m_pos += MoveHere;
                         return;
                     }
             }
 
-            if (lvl.Map[IAM.X + MoveHere.X, IAM.Y + MoveHere.Y] == 0)
+            if (lvl.Map[Position.X + MoveHere.X, Position.Y + MoveHere.Y] == 0)
             {
-                m_pos += MoveHere.ToVector2() * 2;
+                m_pos += MoveHere;
                 return;
             }
         }
@@ -87,10 +77,6 @@ namespace Once
 
     class Player : Actor
     {
-        public Point IAMHERE
-        {
-            get { return IAM; }
-        }
 
         public Player()
             :base(new Rectangle(0,0,14,14),Color.Red, 0)
@@ -116,7 +102,7 @@ namespace Once
         public override void DrawMe(SpriteBatch sb)
         {
             base.DrawMe(sb);
-            sb.DrawString(Font, IAM.X + "," + IAM.Y, Position, Color.White);
+            sb.DrawString(Font, Position.X + "," + Position.Y, new Vector2(Rect.X,Rect.Y), Color.White);
         }
     }
 }
